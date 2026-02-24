4955a639285dc29b5df4a39ac711fea1b39b45b6
Arpit Agnihotri <arpit151604@gmail.com>
2026-02-24 10:34:22 +0530

    minor changes

--- Assignment.js
+++ Assignment.js
@@ -1,16 +1,11 @@
-// Predefined line break for console output
 const newline = "\n";
-
-// Variables
 let a = 10;
 let b = 20;
 
-// Display values
 console.log("Value of a: " + a);
 console.log("Value of b: " + b);
 console.log(newline);
 
-// Functions
 function add(a, b) {
     let res = a + b;
     console.log("The sum of " + a + " and " + b + " is: " + res + newline);
@@ -36,7 +31,6 @@ function mod(a, b) {
     console.log("The modulo of " + a + " and " + b + " is: " + res + newline);
 }
 
-// Function calls
 add(a, b);
 sub(a, b);
 pro(a, b);
