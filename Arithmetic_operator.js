de20d5671358c173abad761fc5e898c9649d1c84
Arpit Agnihotri <arpit151604@gmail.com>
2026-02-25 19:58:57 +0530

    Loop and Arithmetic Operators

---
+++ Arithmetic_operator.js
@@ -0,0 +1,27 @@
+let x = 10, y = 3;
+console.log("Value of x: " + x);
+console.log("Value of y: " + y);
+//document.write("Value of x: " + x + "<br>");
+//document.write("Value of y: " + y + "<br><br>");
+console.log(x + y);
+//document.write("x + y = " + (x + y) + "<br>");
+console.log(x - y);  // 7
+//document.write("x - y = " + (x - y) + "<br>");
+console.log(x * y);  // 30
+//document.write("x * y = " + (x * y) + "<br>");
+console.log(x / y);  // 3.333...
+//document.write("x / y = " + (x / y) + "<br>");
+console.log(x % y);  // 1
+//document.write("x % y = " + (x % y) + "<br><br>");
+
+let z = 5;
+console.log(z);     // 5
+//document.write("Initial value of z = " + z + "<br><br>");
+console.log(++z);    // 6
+//document.write("After pre-increment, z = " + z + "<br>");
+console.log(--z);    // 5
+//document.write("After pre-decrement, z = " + z + "<br>");
+console.log(z++);    // 5 
+//document.write("After post-increment, z = " + z + "<br>");
+console.log(z--);    // 6 
+//document.write("After post-decrement, z = " + z + "<br>");
